af39341e5166ac6d422c0d71c61f2901641a0a23
stevengalvis <steven.galvis@gmail.com>
2018-05-15 18:50:20 -0400

    added Array implementation in JavsScript

---
+++ Data-Structures/Array/memory.js
@@ -0,0 +1,41 @@
+let memory = new Float64Array(1024);
+let head = 0;
+
+let allocate = function(size) {
+  if (head + size > memory.length) {
+    return null;
+  }
+  let start = head;
+  head += size;
+  return start;
+};
+
+let free = function(ptr) {};
+
+let copy = function(to, from, size) {
+  if (from === to) {
+    return;
+  } else if (from > to) {
+    for (let i = 0; i < size; i++) {
+      set(to + i, get(from + i));
+    }
+  } else {
+    for (let i = size - 1; i >= 0; i--) {
+      set(to + i, get(from + i));
+    }
+  }
+};
+
+let get = function(ptr) {
+  return memory[ptr];
+};
+
+let set = function(ptr, value) {
+  memory[ptr] = value;
+};
+
+exports.allocate = allocate;
+exports.free = free;
+exports.copy = copy;
+exports.get = get;
+exports.set = set;
