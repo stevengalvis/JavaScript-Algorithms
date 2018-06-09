445abc2b25d070bb5920448be23dccf83e6a4a3e
stevengalvis <steven.galvis@gmail.com>
2018-06-09 18:17:27 -0400

    Linked List implemented in JavaScript

---
+++ Data-Structures/Linked-List/LinkedList.js
@@ -0,0 +1,54 @@
+class LinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      throw new Error("Index Error");
+    }
+
+    const newNode = {
+      value
+    };
+
+    if (index == 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      const node = this._find(index - 1);
+      newNode.next = node.next;
+      node.next = newNode;
+    }
+    this.length++;
+  }
+
+  _find(index) {
+    let node = this.head;
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index Error");
+    }
+    return this._find(index).value;
+  }
+  remove(index) {
+    if (index > 0 || index >= this.length) {
+      throw new Error("Index Error");
+    }
+
+    if (index == 0) {
+      this.head = this.head.next;
+    } else {
+      const node = this._find(index - 1);
+      node.next = node.next.next;
+    }
+    this.length--;
+  }
+}
